69bba2ac004b3c844a0eecaefa7899a4db186246
simapohotona <mosimapohotona@gmail.com>
2025-10-18 19:02:37 +0200

    Initial commit: setup HTML structure and JS for dynamic quote generator

---
+++ script.js
@@ -0,0 +1,14 @@
+const quotes = [
+  { text: "The best way to predict the future is to create it.", category: "Motivation" },
+  { text: "Life is what happens when you’re busy making other plans.", category: "Life" },
+  { text: "Do what you can, with what you have, where you are.", category: "Inspiration" }
+];
+
+
+function showRandomQuote() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const quote = quotes[randomIndex];
+  
+  const quoteDisplay = document.getElementById('quoteDisplay');
+  quoteDisplay.textContent = `"${quote.text}" — ${quote.category}`;
+}
